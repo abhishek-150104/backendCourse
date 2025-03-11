f2636838be9263c6744f3bcc9606174274fcc6ec
abhishek-150104 <abhishekkumarshrivastav61@gmail.com>
2025-03-11 19:11:26 +0530

    Add Routes

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 require('dotenv').config()
 const express = require('express')  //import express from "express"
 const app = express()
-const port = 3000
-app.get('/twitter', (req, res) => {
+// const port = 3000
+app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+app.get('/twitter', (req, res) => {
+  res.send('Hello Twitter!')
+})
+app.get('/youtube', (req, res) => {
+  res.send('Hello Youtube!')
+})
+app.get('/facebook', (req, res) => {
+  res.send('Hello Facebook!')
 })
 
 app.listen(process.env.PORT, () => {
